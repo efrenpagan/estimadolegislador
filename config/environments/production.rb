868e959d840472513a3161e458f6b0faad227e34
Roberto C. Rivera <roberto@devstud.io>
2015-07-04 23:50:12 -0400

    Fix assets for production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,12 @@ Rails.application.configure do
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(:mangle => false)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
