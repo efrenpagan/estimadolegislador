cac2dbe53c37af429feee771aa0f040c6dd3d999
Roberto C. Rivera <roberto@devstud.io>
2015-07-05 21:32:45 -0400

    Include s3_host_name for AWS S3 bucket

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,8 @@ module Atentamente
         :s3_credentials => {
           :bucket => ENV['s3_bucket'],
           :access_key_id => ENV['s3_access_key_id'],
-          :secret_access_key => ENV['s3_secret_access_key']
+          :secret_access_key => ENV['s3_secret_access_key'],
+          :s3_host_name => ENV['s3_host_name']
         }
       }
     end
