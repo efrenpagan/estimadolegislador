15104e9ce38690f243dbee90d25d41b47fd4cb68
Roberto C. Rivera <roberto@devstud.io>
2015-12-21 23:30:43 -0400

    Fix email headers

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -10,7 +10,7 @@ class MessageMailer < ActionMailer::Base
   def individual_message(message, contact)
     @contact = contact
     @message = message
-    headers { unique_args: { recipient_id: @message.recipient.id } }
+    headers(:unique_args => { :recipient_id => @message.recipient.id })
     mail(to: @contact.email, subject: @message.subject)
   end
 end
